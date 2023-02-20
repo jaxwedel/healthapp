1a31d366600f5a409a59ba2bef0a0aa69e7ed5a6
Johan Wedel <123654226+j0hanw3del@users.noreply.github.com>
2023-02-20 15:16:23 +0100

    Update to Form
    Cleaning up a little

--- src/form.js
+++ src/form.js
@@ -1,33 +1,53 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Alert from '@mui/material/Alert';
 
+const CustomTextField = ({ label, value, onChange, error, helperText }) => {
+    return (
+        <TextField
+            label={label}
+            variant="outlined"
+            type="number"
+            value={value}
+            onChange={onChange}
+            error={error}
+            helperText={helperText}
+        />
+    );
+}
+
+const isValueInRange = (value, min, max) => {
+    return value >= min && value <= max;
+}
+
+const SPACING = 2;
+
 const Form = () => {
-    const [valueTemperature, setValueTemperature] = React.useState('');
-    const [valueHeartRate, setValueHeartRate] = React.useState('');
-    const [valueRespiratoryRate, setValueRespiratoryRate] = React.useState('');
-    const [newsScore, setNewsScore] = React.useState(0);
-    const [temperatureError, setTemperatureError] = React.useState(false);
-    const [heartRateError, setHeartRateError] = React.useState(false);
-    const [respiratoryRateError, setRespiratoryRateError] = React.useState(false);
+    const [valueTemperature, setValueTemperature] = useState('');
+    const [valueHeartRate, setValueHeartRate] = useState('');
+    const [valueRespiratoryRate, setValueRespiratoryRate] = useState('');
+    const [newsScore, setNewsScore] = useState(0);
+    const [temperatureError, setTemperatureError] = useState(false);
+    const [heartRateError, setHeartRateError] = useState(false);
+    const [respiratoryRateError, setRespiratoryRateError] = useState(false);
 
     const handleSubmit = async (e) => {
-        e.preventDefault(); // Prevent page reload
+        e.preventDefault();
         setTemperatureError(false);
         setHeartRateError(false);
         setRespiratoryRateError(false);
 
-        if ( // This is error handling
-            (valueTemperature < 31 || valueTemperature > 42) ||
-            (valueHeartRate < 25 || valueHeartRate > 220) ||
-            (valueRespiratoryRate < 3 || valueRespiratoryRate > 60)
+        if (
+            !isValueInRange(valueTemperature, 31, 42) ||
+            !isValueInRange(valueHeartRate, 25, 220) ||
+            !isValueInRange(valueRespiratoryRate, 3, 60)
         ) {
-            setTemperatureError(valueTemperature < 31 || valueTemperature > 42);
-            setHeartRateError(valueHeartRate < 25 || valueHeartRate > 220);
-            setRespiratoryRateError(valueRespiratoryRate < 3 || valueRespiratoryRate > 60);
+            setTemperatureError(!isValueInRange(valueTemperature, 31, 42));
+            setHeartRateError(!isValueInRange(valueHeartRate, 25, 220));
+            setRespiratoryRateError(!isValueInRange(valueRespiratoryRate, 3, 60));
         } else {
             const response = await fetch('/api/calculate', {
                 method: 'POST',
@@ -55,50 +75,51 @@ const Form = () => {
     return (
         <Box
             component="form"
-            m={2} p={2}
+            m={SPACING}
+            p={SPACING}
             noValidate
-            autoComplete="off">
-            <Box display="flex" flexDirection="column" mb={2}>
-                <Typography variant='h2' color={"primary"} pb={2} >Body Temperature</Typography>
-                <TextField
+            autoComplete="off"
+            onSubmit={handleSubmit}
+        >
+            <Box display="flex" flexDirection="column" mb={SPACING}>
+                <Typography variant='h2' color={"primary"} pb={SPACING} >Body Temperature</Typography>
+                <CustomTextField
                     label="Degrees Celcius"
-                    variant="outlined"
                     value={valueTemperature}
                     onChange={(e) => setValueTemperature(e.target.value)}
                     error={temperatureError}
                     helperText={temperatureError ? 'Temperature must be between 31 and 42' : ''}
                 />
             </Box>
-            <Box display="flex" flexDirection="column" mb={2} pt={2}>
-                <Typography variant='h2' color={"primary"} pb={2} >Heart Rate</Typography>
-                <TextField
+            <Box display="flex" flexDirection="column" mb={SPACING} pt={SPACING}>
+                <Typography variant='h2' color={"primary"} pb={SPACING} >Heart Rate</Typography>
+                <CustomTextField
                     label="Beats per minute"
-                    variant="outlined"
                     value={valueHeartRate}
                     onChange={(e) => setValueHeartRate(e.target.value)}
                     error={heartRateError}
-                    helperText={heartRateError ? 'Heart Rate must be between 25 and 220' : ''} />
+                    helperText={heartRateError ? 'Heart Rate must be between 25 and 220' : ''}
+                />
             </Box>
-            <Box display="flex" flexDirection="column" mb={2} pt={2}>
-                <Typography variant='h2' color={"primary"} pb={2} >Respiratory Rate</Typography>
-                <TextField
+            <Box display="flex" flexDirection="column" mb={SPACING} pt={SPACING}>
+                <Typography variant='h2' color={"primary"} pb={SPACING} >Respiratory Rate</Typography>
+                <CustomTextField
                     label="Breaths per minute"
-                    variant="outlined"
                     value={valueRespiratoryRate}
                     onChange={(e) => setValueRespiratoryRate(e.target.value)}
                     error={respiratoryRateError}
                     helperText={respiratoryRateError ? 'Respiratory Rate must be between 3 and 60' : ''}
                 />
             </Box>
-            <Box display="flex" flexDirection="row" justifyContent={'space-between'} mt={4}>
-                <Button variant="contained" style={{ width: "64%", padding: "12px" }} type="submit" onClick={handleSubmit}>
+            <Box display="flex" flexDirection="row" justifyContent={'space-between'} mt={SPACING * 2}>
+                <Button variant="contained" style={{ width: "64%", padding: "12px" }} type="submit">
                     Calculate NEWS
                 </Button>
                 <Button variant="outlined" style={{ width: "34%", padding: "12px" }} type="button" onClick={handleClear}>
                     Reset
                 </Button>
             </Box>
-            <Box display="flex" mt={4} sx={{ height: "50px" }}>
+            <Box display="flex" mt={SPACING * 2} sx={{ height: "50px" }}>
                 {(
                     newsScore !== 0 && (
                         <Alert severity="success" sx={{ width: "100%" }}>
@@ -111,7 +132,7 @@ const Form = () => {
         </Box>
 
     );
+
 };
 
 export default Form;
-
